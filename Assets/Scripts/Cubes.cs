d158846c50c25972109de6a8ee7c8118f3534b34
TrapReaper <jorgel.iglesias@hotmail.com>
2018-02-13 20:29:32 -0500

    Parcial 1.1
    Es mejor prevenir que lamentar

---
+++ Assets/Scripts/Cubes.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes : MonoBehaviour
+{
+    [SerializeField]
+    private int cube;
+
+    [SerializeField]
+    private GameObject cubo;
+
+    private Collider destruction;
+
+    private int die;
+
+    private void Start()
+    {
+        destruction = GetComponent<Collider>();
+        die = 0;
+
+        if(cube == 1)
+        {
+            Renderer rend = GetComponent<Renderer>();
+            
+            rend.material.SetColor("_Color", Color.red);
+        }
+        if(cube == 2)
+        {
+            Renderer rend = GetComponent<Renderer>();
+            
+            rend.material.SetColor("_Color", Color.blue);
+        }
+        if(cube == 3)
+        {
+            Renderer rend = GetComponent<Renderer>();
+            
+            rend.material.SetColor("_Color", Color.green);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Ball ball = collision.gameObject.GetComponent<Ball>();
+
+            if (ball != null)
+            {
+                if(cube == 1)
+                {
+                    if(die >= 1)
+                    {
+                        DestroyObject(cubo);
+                    }
+                    else
+                    {
+                        die += 1;
+                    }
+                }
+
+                if (cube == 2)
+                {
+                    if (die >= 2)
+                    {
+                        DestroyObject(cubo);
+                    }
+                    else
+                    {
+                        die += 1;
+                    }
+                }
+
+                if (cube == 3)
+                {
+                    if (die >= 3)
+                    {
+                        DestroyObject(cubo);
+                    }
+                    else
+                    {
+                        die += 1;
+                    }
+                }
+            }
+
+        }
+    }
+
+
+		
+}
---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject life1;
+
+    [SerializeField]
+    private GameObject life2;
+
+
+    [SerializeField]
+    private GameObject life3;
+
+    [SerializeField]
+    private Collider death;
+
+    private int life;
+
+    private void Start()
+    {
+        death = GetComponent<Collider>();
+        life = 2;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Ball ball = collision.gameObject.GetComponent<Ball>();
+
+            if (ball != null)
+            {
+                if(life < 0)
+                {
+                    Debug.Log("colision0");
+                }
+                if(life == 2)
+                {
+                    life -= 1;
+                    DestroyObject(life1);
+                    Debug.Log("colision1");
+                }
+                if (life == 1)
+                {
+                    life -= 1;
+                    DestroyObject(life2);
+                    Debug.Log("colision2");
+                }
+                if (life == 0)
+                {
+                    life -= 1;
+                    DestroyObject(life3);
+                    Debug.Log("colision3");
+                }
+
+            }
+        }
+    }
+
+                // Update is called once per frame
+                void Update () {
+		
+	}
+}
